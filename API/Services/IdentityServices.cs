61246f1bdcd1292ff144aa0d60e9b7ab70bfb62c
JesseRM <jesserrm@gmail.com>
2023-09-02 22:56:48 -0700

    Add Azure B2C info

--- API/Services/IdentityServices.cs
+++ API/Services/IdentityServices.cs
@@ -1,15 +1,36 @@
 ﻿using API.Authentication.Basic;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
 
 namespace API.Services
 {
     public static class IdentityServices
     {
-        public static IServiceCollection AddIdentityServices(this IServiceCollection services)
+        public static IServiceCollection AddIdentityServices(
+            this IServiceCollection services,
+            IConfiguration config
+        )
         {
             services
                 .AddAuthentication()
                 .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("Basic", null);
+
+            // Adds Microsoft Identity platform (Azure AD B2C) support to protect this Api
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApi(
+                    options =>
+                    {
+                        config.Bind("AzureAdB2C", options);
+
+                        options.TokenValidationParameters.NameClaimType = "name";
+                    },
+                    options =>
+                    {
+                        config.Bind("AzureAdB2C", options);
+                    }
+                );
 
             return services;
         }
