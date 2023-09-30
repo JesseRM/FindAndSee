930d75036261c6055abc4cb51aa5f7fb19db1d5b
JesseRM <jesserrm@gmail.com>
2023-09-29 17:42:21 -0700

    Add mapping profiles

---
+++ Application/Core/MappingProfiles.cs
@@ -0,0 +1,15 @@
+﻿using Application.Finds;
+using AutoMapper;
+using Domain;
+
+namespace Application.Core
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Find, FindBasicDto>();
+            CreateMap<Find, FindDetailsDto>();
+        }
+    }
+}
